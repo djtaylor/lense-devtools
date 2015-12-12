f8a144de45f6906920464a9482d2de41249a972c
David Taylor <djtaylor13@gmail.com>
2015-12-12 08:27:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ INSTALLER=$WORKSPACE/install
 
 # Must be root
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+    echo "This script must be run as root" 1>&2
+    exit 1
 fi
 
 # Make sure the install directory is free
 if [ -d ${WORKSPACE} ] && [ $(ls -A ${WORKSPACE}) ]; then
-	echo "ERROR: Workspace directory <${WORKSPACE}> already exists"
-	echo "INFO: Change the WORKSPACE variable or clear the directory"
-	exit 1
+    echo "ERROR: Workspace directory <${WORKSPACE}> already exists"
+    echo "INFO: Change the WORKSPACE variable or clear the directory"
+    exit 1
 fi
 mkdir -p $INSTALLER
 
@@ -26,13 +26,16 @@ apt-get install build-essential devscripts git
 apt-get install python-pip
 pip install GitPython
 
-# Get the soource code
+# ~/.lense_devtools/install
 cd $INSTALLER
+
+# Get the latest source code
 git clone https://github.com/djtaylor/lense-devtools.git
-cd lense_devtools
 
 # Tar the source directory
 tar czf lense-devtools_0.1.1.orig.tar.gz lense-devtools
+
+# ~/.lense_devtools/install/lense-devtools
 cd lense-devtools
 
 # Build the package without signing
